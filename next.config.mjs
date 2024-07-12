fd4c216482d6b9e6b09be7d203ae274515fd236f
Aryan Chaturvedi <mandaknidevi04@gmail.com>
2024-07-12 22:02:53 +0530

    dne

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-    ignoreBuildErrors: true,
-  },
-};
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
